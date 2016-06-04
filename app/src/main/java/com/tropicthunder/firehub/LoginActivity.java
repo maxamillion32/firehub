771506c9033fa75db0558660d6e1e36edabc972a
Gareth Teoh <garethteoh93@gmail.com>
2016-06-04 11:39:17 +0800

    firbase testing

--- app/src/main/java/com/tropicthunder/firehub/LoginActivity.java
+++ app/src/main/java/com/tropicthunder/firehub/LoginActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +15,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_login);
+
+        Firebase ref = new Firebase("https://firehub-5ae48.firebaseio.com");
+        ref.createUser("bryanlee@firebase.com", "imbryanlee", new Firebase.ValueResultHandler<Map<String, Object>>() {
+            @Override
+            public void onSuccess(Map<String, Object> result) {
+                System.out.println("Successfully created user account with uid: " + result.get("uid"));
+            }
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                // there was an error
+                System.out.println("Error");
+            }
+        });
     }
 }
