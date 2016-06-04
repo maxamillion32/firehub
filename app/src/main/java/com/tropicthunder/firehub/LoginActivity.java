c50de1ca36c4cf51a365a2261f56bcefb5d7cd76
bryanleekm <bryanleekm@gmail.com>
2016-06-04 13:43:55 +0800

    Firebase Setup

--- app/src/main/java/com/tropicthunder/firehub/LoginActivity.java
+++ app/src/main/java/com/tropicthunder/firehub/LoginActivity.java
@@ -1,12 +1,18 @@
 package com.tropicthunder.firehub;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-
-import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,17 +22,54 @@ public class LoginActivity extends AppCompatActivity {
         Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_login);
 
-        Firebase ref = new Firebase("https://firehub-ahkl.firebaseio.com");
-        ref.createUser("bryanlee@firebase.com", "imbryanlee", new Firebase.ValueResultHandler<Map<String, Object>>() {
+        final Firebase ref = new Firebase("https://firehub-ahkl.firebaseio.com");
+
+        Button btnLogin = (Button)findViewById(R.id.btnLogin);
+        Button btnRegister = (Button) findViewById(R.id.btnRegister);
+        final EditText txtUsername = (EditText) findViewById(R.id.txtEmail);
+        final EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+
+        final Firebase.AuthResultHandler authResultHandler = new Firebase.AuthResultHandler() {
             @Override
-            public void onSuccess(Map<String, Object> result) {
-                System.out.println("Successfully created user account with uid: " + result.get("uid"));
+            public void onAuthenticated(AuthData authData) {
+                // Authenticated successfully with payload authData
+                Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
             }
             @Override
-            public void onError(FirebaseError firebaseError) {
-                // there was an error
-                System.out.println("Error" + firebaseError.getMessage());
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                // Authenticated failed with error firebaseError
+                Toast.makeText(getApplicationContext(), firebaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Hide keyboard
+                InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                //Data validation
+                if (txtUsername.getText().toString().length()!=0 && txtPassword.getText().toString().length()!=0) {
+                    ref.authWithPassword(txtUsername.getText().toString(), txtPassword.getText().toString(), authResultHandler);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Complete all fields!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/tropicthunder/firehub/RegistrationActivity.java
@@ -0,0 +1,98 @@
+package com.tropicthunder.firehub;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.Map;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        final Firebase ref = new Firebase("https://firehub-ahkl.firebaseio.com");
+
+        Button btnRegister = (Button)findViewById(R.id.btnRegister);
+        final EditText txtEmail = (EditText) findViewById(R.id.txtEmail);
+        final EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+        final EditText txtRepeatPassword = (EditText) findViewById(R.id.txtRepeatPassword);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Hide keyboard
+                InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+
+                final String email = txtEmail.getText().toString();
+                final String password = txtPassword.getText().toString();
+                String repeatPassword = txtRepeatPassword.getText().toString();
+
+                final Firebase.AuthResultHandler authResultHandler = new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        // Authenticated successfully with payload authData
+                        Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                        finish();
+                    }
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        // Authenticated failed with error firebaseError
+                        Toast.makeText(getApplicationContext(), firebaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                };
+
+                //Data validation
+                if (email.length()!= 0 && password.length()!= 0 && repeatPassword.length()!= 0){
+                    if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                        Toast.makeText(getApplicationContext(), "Please enter a valid email", Toast.LENGTH_SHORT).show();
+                    } else if (!password.equals(repeatPassword)){
+                        Toast.makeText(getApplicationContext(), "Your passwords do not match!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        //Valid inputs, try to register
+                        ref.createUser(email, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
+                            @Override
+                            public void onSuccess(Map<String, Object> result) {
+                                Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_SHORT).show();
+                                ref.authWithPassword(email, password, authResultHandler);
+                            }
+                            @Override
+                            public void onError(FirebaseError firebaseError) {
+                                // there was an error
+                                Toast.makeText(getApplicationContext(), firebaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                                System.out.println("Error" + firebaseError.getMessage());
+                            }
+                        });
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please ensure all fields are filled up!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+
+
+        });
+
+
+
+    }
+
+
+}
