8e2f27269188f6c27ee94cc4214d55ee79f8c4d9
bryanleekm <bryanleekm@gmail.com>
2016-06-04 11:31:33 +0800

    Firebase Setup

--- app/src/main/java/com/tropicthunder/firehub/LoginActivity.java
+++ app/src/main/java/com/tropicthunder/firehub/LoginActivity.java
@@ -3,11 +3,14 @@ package com.tropicthunder.firehub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_login);
     }
 }
